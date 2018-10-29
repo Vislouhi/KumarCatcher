fd7475c01988797231f308722b4972b7d6e0020a
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2018-10-29 14:36:02 +0000

    Update randomScene.java

--- src/randomScene.java
+++ src/randomScene.java
@@ -1,1 +1,9 @@
 
+public class RandomView{
+ 
+  private int numberOfQubes=3;
+  private SceneQubeData[][][] sceneQubes = new SceneQubeData[numberOfQubes][numberOfQubes][numberOfQubes];
+ RandomView
+  
+  
+}
