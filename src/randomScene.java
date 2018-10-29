027e8f3c754dd450118885dc9232c97856493271
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2018-10-29 14:55:40 +0000

    Update randomScene.java

--- src/randomScene.java
+++ src/randomScene.java
@@ -2,8 +2,37 @@
 public class RandomView{
  
   private int numberOfQubes=3;
-  private SceneQubeData[][][] sceneQubes = new SceneQubeData[numberOfQubes][numberOfQubes][numberOfQubes];
- RandomView
+  private SceneQubeData[][][] sceneQubes = new SceneQubeData[GameParameters.numberOfQubes][GameParameters.numberOfQubes][GameParameters.numberOfQubes];
   
+  public RandomView(){
+   for(int i=0;i<GameParameters.numberOfQubes;i++){
+     for(int j=0;j<GameParameters.numberOfQubes;j++){
+       for(int k=0;i<GameParameters.numberOfQubes;k++){
+        
+        sceneQubes[i][j][k]=new SceneQubeData(i,j,k);
+    
+       }
+     }
+   }
   
+  }
+}
+
+public class SceneQubeData
+{
+ private float x;
+ private float y;
+ private float z;
+ 
+ public SceneQubeData(int i, int j, int k)
+ {
+   x= i* GameParameters.sceneCubeSize+
+  
+ }
+}
+
+public class GameParameters
+{
+ static float sceneCubeSize =10f;
+ static int numberOfQubes=3;
 }
